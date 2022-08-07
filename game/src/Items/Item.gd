9fe0b86e3beceed1c2d700c00c077a7eea374cb2
alexg-lviv <alexg.lviv@gmail.com>
2022-08-07 14:21:28 +0300

    add 2 powerups and cool coins

--- game/src/Items/Item.gd
+++ game/src/Items/Item.gd
@@ -2,10 +2,25 @@ extends Area2D
 
 class_name Item
 
-func _on_Coin_area_entered(area):
-	if area.is_in_group("player"):
-		$AnimationPlayer.play("fadeout")
+var _type: String
+
+var PowerupsSprites: Dictionary = {
+	"freeze": "res://art/pickups/freeze.png",
+	"speed": "res://art/pickups/speedup.png"
+}
+
+func set_type(type):
+	_type = type
+	$Sprite.texture = load(PowerupsSprites[type])
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "fadein":
 		$AnimationPlayer.play("float")
+
+
+func _on_Item_area_entered(area):
+	if area.is_in_group("player"):
+		$AnimationPlayer.play("fadeout")
+
+func get_type():
+	return _type
